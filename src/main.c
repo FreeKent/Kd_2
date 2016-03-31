d917ebae2f837f5614a57e79c7d884a0dd579cd7
Никита Дубов <freekent1@mail.ru>
2016-03-31 06:44:29 +0600

    auto

--- src/main.c
+++ src/main.c
@@ -160,12 +160,12 @@ int main (int argc, char *argv[]) {
     close(fdout[1]);
     close(fderr[1]);
     close(fdin[0]);
-    fcntl(0, F_SETFL, O_ASYNC | NONBLOCK);
+    fcntl(0, F_SETFL, O_ASYNC | O_NONBLOCK);
     fcntl(0, F_SETSIG, SIGIO);
     fcntl(fdout[0], F_SETFL, O_ASYNC);
-    fcntl(fdout[0], F_SETSIG, SIGIO | NONBLOCK);
+    fcntl(fdout[0], F_SETSIG, SIGIO | O_NONBLOCK);
     fcntl(fderr[0], F_SETFL, O_ASYNC);
-    fcntl(fderr[0], F_SETSIG, SIGIO | NONBLOCK);
+    fcntl(fderr[0], F_SETSIG, SIGIO | O_NONBLOCK);
     
     while (!receivedChildSignal) {
       sleep(1);
